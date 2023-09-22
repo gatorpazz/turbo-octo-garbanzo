6e18b18eefa5a0538990feb3617baf91855241dd
Michael Pascuzzi <gatorpazz@gmail.com>
2023-09-21 20:51:48 -0400

    Setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import Profile from './pages/Profile'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Posts from './pages/Posts'
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<h1>Posts</h1>} />
-        <Route path="/profile" element={<h1>Profile</h1>} />
-        <Route path="/login" element={<h1>Login</h1>} />
-        <Route path="/register" element={<h1>Register</h1>} />
+        <Route path="/" element={<Posts />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   )
