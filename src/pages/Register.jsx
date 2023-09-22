faede137cb3ebe44b53a7158a3d21b18f06e8d76
Michael Pascuzzi <gatorpazz@gmail.com>
2023-09-21 21:49:47 -0400

    Register and login setting token state

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,32 @@
-const Register = () => {
+import { useState } from 'react';
+import { registerUser } from '../API';
+
+const Register = ({setToken}) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const token = await registerUser(username, password);
+    setToken(token);
+  }
   return (
     <div>
       <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Register</button>
+      </form>
     </div>
   )
 }
